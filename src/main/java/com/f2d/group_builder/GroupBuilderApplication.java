81ed14d688881407407c6172ded57fb2c0be14f4
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-09-16 00:08:47 -0400

    more changes

--- src/main/java/com/f2d/group_builder/GroupBuilderApplication.java
+++ src/main/java/com/f2d/group_builder/GroupBuilderApplication.java
@@ -2,9 +2,11 @@ package com.f2d.group_builder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GroupBuilderApplication {
 
 	public static void main(String[] args) {
