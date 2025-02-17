fabc92079afdb2bb03980ab6fd94ba2196e563aa
Ejdeguzman72 <ejdeguzman72@gmail.com>
2025-02-16 23:27:15 -0500

    more changes

--- src/main/java/com/f2d/group_builder/feign/F2DChatroomClient.java
+++ src/main/java/com/f2d/group_builder/feign/F2DChatroomClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = AppConstant.F2D_CHATROOM,
-        url = AppConstant.LOCALHOST,
+        url = AppConstant.RASPBERRY_PI_F2D,
         configuration = FeignConfig.class,
         fallback = F2DChatroomClientFallback.class)
 public interface F2DChatroomClient {
